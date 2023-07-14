55343405b9516ce7c375c84d5dbd424270a9fd21
Niklas Schäfer <niklasanonym4@gmail.com>
2023-07-14 18:42:26 +0200

    Added Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'tictactoeangular-6291d',
+    appId: '1:8507815058:web:8fcb83d040dcbf117e624b',
+    storageBucket: 'tictactoeangular-6291d.appspot.com',
+    apiKey: 'AIzaSyCq_HtOgZn0KaEG6emfbbWlvb6vh9GsR4U',
+    authDomain: 'tictactoeangular-6291d.firebaseapp.com',
+    messagingSenderId: '8507815058',
+    measurementId: 'G-HPLD7K9HQS',
+  },
+};
