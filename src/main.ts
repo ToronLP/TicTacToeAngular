1afb279dcd7a21a14d4095c4a2ec5c40405bb8b8
Toron <niklasanonym4@gmail.com>
2023-07-15 00:46:07 +0200

    Added dowload Message

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/ngsw-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered:', registration);
+      })
+      .catch(error => {
+        console.error('Error registering Service Worker:', error);
+      });
+  });
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
