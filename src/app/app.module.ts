931d8a985e205f0096cdb2ee8443c11cd5cc3860
Toron <niklasanonym4@gmail.com>
2023-07-15 14:34:49 +0200

    Added Bootstrap to Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { BoardComponent } from './board/board.component';
 import { SquareComponent } from './square/square.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { SquareComponent } from './square/square.component';
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
