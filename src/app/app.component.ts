2f3ccc9ee1e04fffa1952e0bae8d340e35698412
Toron <niklasanonym4@gmail.com>
2023-07-16 22:01:54 +0200

    Test for installation prompt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 
 @Component({
@@ -6,15 +6,30 @@ import { SwUpdate } from '@angular/service-worker';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TicTacToeAngular';
   
   updateAvailable: boolean;
+  installPrompt = event;
+  installButton = document.querySelector("#install");
 
   constructor(private update: SwUpdate){
     this.updateAvailable = false;
     this.updateClient();
   }
+
+  ngOnInit() {
+    window.addEventListener('beforeinstallprompt', (event: Event) => {
+      event.preventDefault(); // Prevent the default prompt
+      const installPrompt = event;
+      // Show a custom install prompt or handle the installation logic
+      // You can use a button or any other UI element to trigger the installation
+    });
+  }
+
+  triggerInstallPrompt() {
+    (window as any).beforeinstallprompt.prompt();
+  }  
 
   updateClient(){
     if(!this.update.isEnabled){
